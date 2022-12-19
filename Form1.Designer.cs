48b6e128ae59f9584ae6f0dccaa88fd79021350f
Shao <F403@ktgh.com.tw>
2022-12-19 08:34:09 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Perspective
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPosXval = new System.Windows.Forms.Label();
+            this.lblPosYVal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Perspective.Properties.Resources.source1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 83);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 574);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(538, 83);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(500, 574);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 35);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "原始圖片";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(538, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 35);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "結果";
+            // 
+            // lblPosXval
+            // 
+            this.lblPosXval.AutoSize = true;
+            this.lblPosXval.Location = new System.Drawing.Point(248, 20);
+            this.lblPosXval.Name = "lblPosXval";
+            this.lblPosXval.Size = new System.Drawing.Size(42, 15);
+            this.lblPosXval.TabIndex = 4;
+            this.lblPosXval.Text = "label3";
+            this.lblPosXval.Visible = false;
+            // 
+            // lblPosYVal
+            // 
+            this.lblPosYVal.AutoSize = true;
+            this.lblPosYVal.Location = new System.Drawing.Point(296, 20);
+            this.lblPosYVal.Name = "lblPosYVal";
+            this.lblPosYVal.Size = new System.Drawing.Size(42, 15);
+            this.lblPosYVal.TabIndex = 5;
+            this.lblPosYVal.Text = "label4";
+            this.lblPosYVal.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 669);
+            this.Controls.Add(this.lblPosYVal);
+            this.Controls.Add(this.lblPosXval);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private Label label2;
+        private Label lblPosXval;
+        private Label lblPosYVal;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace Perspective
+{
+    public partial class Form1 : Form
+    {
+        double[,] A = new double[8, 8];
+        double[] b = new double[8];
+        Bitmap image1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            image1 = new Bitmap(pictureBox1.Image);
+
+            // points
+            int x1 = 0, x2 = 360, x3 = 0, x4 = 360;
+            int y1 = 0, y2 = 0, y3 = 450, y4 = 450;
+
+            int xx1 = 689, xx2 = 2407, xx3 = 187, xx4 = 2945;
+            int yy1 = 532, yy2 = 477, yy3 = 2703, yy4 = 2661;
+
+            Matrix<double> A = Matrix<double>.Build.DenseOfArray(new double[,] {
+                { x1, y1, x1*y1, 1, 0, 0, 0, 0},
+                { x2, y2, x2*y2, 1, 0, 0, 0, 0},
+                { x3, y3, x3*y3, 1, 0, 0, 0, 0},
+                { x4, y4, x4*y4, 1, 0, 0, 0, 0},
+                { 0, 0, 0, 0, x1, y1, x1*y1, 1},
+                { 0, 0, 0, 0, x2, y2, x2*y2, 1},
+                { 0, 0, 0, 0, x3, y3, x3*y3, 1},
+                { 0, 0, 0, 0, x4, y4, x4*y4, 1}
+            });
+            Vector<double> b = Vector<double>.Build.Dense(new double[] { xx1, xx2, xx3, xx4, yy1, yy2, yy3, yy4 });
+            // 參數計算
+            var result = A.Solve(b);
+
+            // 內插
+            Bitmap bmp = new Bitmap(360, 450);
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    var double_x = (result[0] * i) + (result[1] * j) + (result[2] * i * j) + result[3];
+                    var double_y = (result[4] * i) + (result[5] * j) + (result[6] * i * j) + result[7];
+                    int y = (int)(double_y);
+                    int x = (int)(double_x);
+                    var v = (double_y - y);
+                    var u = (double_x - x);
+
+                    var R = (1 - u) * (1 - v) * image1.GetPixel(x, y).R
+                        + u * (1 - v) * image1.GetPixel(x, y + 1).R
+                        + v * (1 - u) * image1.GetPixel(x + 1, y).R
+                        + u * v * image1.GetPixel(x + 1, y + 1).R;
+                    var G = (1 - u) * (1 - v) * image1.GetPixel(x, y).G
+                        + u * (1 - v) * image1.GetPixel(x, y + 1).G
+                        + v * (1 - u) * image1.GetPixel(x + 1, y).G
+                        + u * v * image1.GetPixel(x + 1, y + 1).G;
+                    var B = (1 - u) * (1 - v) * image1.GetPixel(x, y).B
+                        + u * (1 - v) * image1.GetPixel(x, y + 1).B
+                        + v * (1 - u) * image1.GetPixel(x + 1, y).B
+                        + u * v * image1.GetPixel(x + 1, y + 1).B;
+
+                    bmp.SetPixel(i, j, Color.FromArgb(Convert.ToInt32(R), Convert.ToInt32(G), Convert.ToInt32(B)));
+                }
+            }
+
+            pictureBox2.Image = bmp;
+        }
+
+        private static System.Drawing.Image resizeImage(System.Drawing.Image imgToResize, Size size)
+        {
+            //Get the image current width  
+            int sourceWidth = imgToResize.Width;
+            //Get the image current height  
+            int sourceHeight = imgToResize.Height;
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+            //Calulate  width with new desired size  
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            //Calculate height with new desired size  
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+            //New Width  
+            int destWidth = (int)(sourceWidth * nPercent);
+            //New Height  
+            int destHeight = (int)(sourceHeight * nPercent);
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((System.Drawing.Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            // Draw image with new width and height  
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+            return (System.Drawing.Image)b;
+        }
+
+        private void ShowCoords(Int32 mouseX, Int32 mouseY)
+        {
+            Int32 realW = pictureBox1.Image.Width;
+            Int32 realH = pictureBox1.Image.Height;
+            Int32 currentW = pictureBox1.ClientRectangle.Width;
+            Int32 currentH = pictureBox1.ClientRectangle.Height;
+            Double zoomW = (currentW / (Double)realW);
+            Double zoomH = (currentH / (Double)realH);
+            Double zoomActual = Math.Min(zoomW, zoomH);
+            Double padX = zoomActual == zoomW ? 0 : (currentW - (zoomActual * realW)) / 2;
+            Double padY = zoomActual == zoomH ? 0 : (currentH - (zoomActual * realH)) / 2;
+
+            Int32 realX = (Int32)((mouseX - padX) / zoomActual);
+            Int32 realY = (Int32)((mouseY - padY) / zoomActual);
+            lblPosXval.Text = realX < 0 || realX > realW ? "-" : realX.ToString();
+            lblPosYVal.Text = realY < 0 || realY > realH ? "-" : realY.ToString();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Default check: left mouse button only
+            if (e.Button == MouseButtons.Left)
+                ShowCoords(e.X, e.Y);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Allows dragging to also update the coords. Checking the button
+            // on a MouseMove is an easy way to detect click dragging.
+            if (e.Button == MouseButtons.Left)
+                ShowCoords(e.X, e.Y);
+        }
+    }
+}
